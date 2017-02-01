54df5684173f0c658f729e6c9529b2bbaea4b70a
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-02-01 12:55:33 +0530

    Refactored test grid bot

--- runGridBot.js
+++ runGridBot.js
@@ -2,33 +2,44 @@ Robot = require('./robot.js');
 Grid = require('./grid.js');
 CommandInterface = require('./commandInterface.js');
 FileInputManager = require('./fileInputManager.js');
-
+var inputManager;
 var inputPath = '/Users/rahulsurabhi/Documents/Training/jsconf-grid-bot/testInput.txt';
+
+function createGrid() {
+  var gridSize = inputManager.getGridSize();
+  var gridX = parseInt(gridSize.x);
+  var gridY = parseInt(gridSize.y);
+  if (isNaN(gridX) || isNaN(gridY)) {
+    console.log('Invalid Grid Inputs');
+    return false;
+  }
+  var grid = new Grid(gridX, gridY);
+  return grid;
+}
+
+function createRobot() {
+  var initialBotInput = inputManager.getBotInputs();
+  var initialX = parseInt(initialBotInput.x);
+  var initialY = parseInt(initialBotInput.y);
+  if (isNaN(initialX) || isNaN(initialX)) {
+    console.log('Invalid Bot Position Input');
+    return false;
+  }
+  console.log(initialX, initialY, initialBotInput.direction);
+
+  var robot = new Robot(initialX, initialY, initialBotInput.direction, ['L', 'M', 'R']);
+  return robot;
+}
 
 function continueExecution(isSuccess) {
   if (!isSuccess) {
     return;
   }
-
-  var gridSize = inputManager.getGridSize();
-  var gridX = parseInt(gridSize.x);
-  var gridY = parseInt(gridSize.y);
-  if(isNaN(gridX)||isNaN(gridY)){
-    console.log('Invalid Grid Inputs');
-    return;
-  }
-  var grid = new Grid(gridX, gridY);
-
-  var initialBotInput = inputManager.getBotInputs();
-  var initialX = parseInt(initialBotInput.x);
-  var initialY = parseInt(initialBotInput.y);
-  if(isNaN(initialX)||isNaN(initialX)){
-    console.log('Invalid Bot Position Input');
-    return;
-  }
-  console.log(initialX,initialY,initialBotInput.direction);
-
-  var robot = new Robot(initialX, initialY, initialBotInput.direction, ['L', 'M', 'R']);
+  
+  var grid = createGrid();
+  if (!grid) return;
+  var robot = createRobot();
+  if (!robot) return;
 
   var commands = inputManager.getCommands();
   var commandInterface = new CommandInterface(robot, grid);
