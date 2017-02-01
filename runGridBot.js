d807c4af86a5cbcb7b243899f0e77ad0d6168a5a
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-02-01 12:40:52 +0530

    updated test grid bot

--- runGridBot.js
+++ runGridBot.js
@@ -5,32 +5,26 @@ FileInputManager = require('./fileInputManager.js');
 
 var inputPath = '/Users/rahulsurabhi/Documents/Training/jsconf-grid-bot/testInput.txt';
 
-function createRobot(positionX, positionY, direction, commands) {
-  var robot = new Robot(positionX, positionY, direction, commands);
-  return robot;
-}
-
-function createGrid(xLimit, yLimit) {
-  var grid = new Robot(xLimit, yLimit);
-  return grid;
-}
-
-function createCommandInterface(robot, grid) {
-  var commandInterface = new CommandInterface(robot, grid);
-  return commandInterface;
-}
-
 function continueExecution(isSuccess) {
   if (!isSuccess) {
     return;
   }
-  // var gridSize = inputManager.getGridSize();
-  var grid = new Grid(4, 4);
 
-  // var initialBotPosition = inputMana.getBotPosition
-  var robot = new Robot(1, 2, 90, ['L', 'R', 'M']);
+  var gridSize = inputManager.getGridSize();
+  var gridX = parseInt(gridSize.x);
+  var gridY = parseInt(gridSize.y)
+  var grid = new Grid(gridX, gridY);
+
+  var initialBotInput = inputManager.getBotInputs();
+  var initialX = parseInt(initialBotInput.x);
+  var initialY = parseInt(initialBotInput.y);
+  console.log(initialX,initialY,initialBotInput.direction);
+
+  var robot = new Robot(initialX, initialY, initialBotInput.direction, ['L', 'M', 'R']);
+
+  var commands = inputManager.getCommands();
   var commandInterface = new CommandInterface(robot, grid);
-  commandInterface.initializeExecution(['L', 'R', 'M', 'M', 'L', 'M', 'R', 'R', 'R'], (err, result) => {
+  commandInterface.initializeExecution(commands, (err, result) => {
     if (err) console.log(err);
     else {
       console.log(result);
