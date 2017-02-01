2f9f87aea8c1e8462f90362624ebba11cce9d52d
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-02-01 12:35:24 +0530

    add methods to switch direction units

--- robot.js
+++ robot.js
@@ -2,8 +2,46 @@ class Robot {
   constructor(positionX, positionY, direction, commands) {
     this.positionX = positionX;
     this.positionY = positionY;
-    this.direction = direction;
     this.commands = commands;
+    this.direction = this.getAngle(direction);
+  }
+
+  getCompassDirection(direction) {
+    var updatedDirection;
+    switch (direction) {
+      case 0:
+        updatedDirection = 'E';
+        break;
+      case 90:
+        updatedDirection = 'N';
+        break;
+      case 180:
+        updatedDirection = 'W';
+        break;
+      case 270:
+        updatedDirection = 'S';
+        break;
+    }
+    return updatedDirection;
+  }
+
+  getAngle(direction) {
+    var updatedDirection;
+    switch (direction) {
+      case 'E':
+        updatedDirection = 0;
+        break;
+      case 'N':
+        updatedDirection = 90;
+        break;
+      case 'W':
+        updatedDirection = 180;
+        break;
+      case 'S':
+        updatedDirection = 270;
+        break;
+    }
+    return updatedDirection;
   }
 
   isValidCommand(input) {
@@ -23,13 +61,12 @@ class Robot {
     var tempDirection = this.direction;
     tempDirection += deltaDirection;
     if (tempDirection < 0) {
-      tempDirection = 360 - tempDirection;
+      tempDirection = 360 + tempDirection;
     }
     if (tempDirection >= 360) {
       tempDirection = tempDirection - 360;
     }
     this.direction = tempDirection;
-    console.log(this.direction);
   }
 
   getPosition() {
